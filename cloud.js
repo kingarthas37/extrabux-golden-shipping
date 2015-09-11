0eb7bfbfef99483feae9ad78eb9a6ec0a0c0eea0
ralf <kingarthas37@hotmail.com>
2015-09-11 11:13:49 +0800

    dev

--- cloud.js
+++ cloud.js
@@ -4,7 +4,11 @@ var AV = require('leanengine');
  * 一个简单的云代码方法
  */
 AV.Cloud.define('hello', function(request, response) {
-  response.success('Hello world!');
+  response.success('Hello world ralf!');
+});
+
+AV.Cloud.define('hello1', function(request, response) {
+    response.success('Hello world ralf!');
 });
 
 module.exports = AV.Cloud;
