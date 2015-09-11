3d88d3416501f2b04fc16f42996473fab3d89069
ralf <kingarthas37@hotmail.com>
2015-09-11 17:19:16 +0800

    dev

--- cloud.js
+++ cloud.js
@@ -17,6 +17,22 @@ AV.Cloud.define('hello', function (request, response) {
                 response.error('查询失败，没有找到匹配的兑换码，请重新输入');
                 return;
             }
+
+
+            var post = new Post();
+            post.set("code", "12345");
+            post.set("type", "222");
+
+            post.save(null, {
+                success: function(post) {
+                    // 成功保存之后，执行其他逻辑.
+                },
+                error: function(post, error) {
+                    // 失败之后执行其他逻辑
+                    // error 是 AV.Error 的实例，包含有错误码和描述信息.
+                }
+            });
+            
             
             data.set('userId',userId);
             data.set('purchaseId',purchaseId);
