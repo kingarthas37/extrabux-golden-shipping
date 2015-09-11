205ce7c1bcccd8dcf2e8cdbe6e940252d5fc6690
ralf <kingarthas37@hotmail.com>
2015-09-11 15:31:10 +0800

    dev

--- cloud.js
+++ cloud.js
@@ -1,21 +1,29 @@
 var AV = require('leanengine');
 
-/**
- * 一个简单的云代码方法
- */
-AV.Cloud.define('hello', function(request, response) {
-  response.success('Hello world ralf!22222');
-});
 
-AV.Cloud.define('hello1', function(request, response) {
-    response.success('Hello world ralf!');
-});
 
-AV.Cloud.define('hello2', function(request, response) {
+AV.Cloud.define('hello', function (request, response) {
+
+
+    var query = new AV.Query('GoldenWeek');
+
+    query.first({
+        success: function(data) {
+            
+            if(!data) {
+                response.error('查询失败，没有找到匹配的兑换码，请重新输入');
+                return;
+            }
+            
+            response.success(data.get('code'));
+            
+        },
+        error: function(error) {
+            response.error("Error: " + error.code + " " + error.message);
+        }
+    });
     
     
-    
-    response.success('Hello world ralfasdfasdf!');
 });
 
 module.exports = AV.Cloud;
