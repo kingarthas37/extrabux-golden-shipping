68cd972b6f12cfe930b47b7608d86fa664480618
ralf <kingarthas37@hotmail.com>
2015-09-11 17:57:31 +0800

    dev

--- cloud.js
+++ cloud.js
@@ -10,36 +10,40 @@ AV.Cloud.define('hello', function (request, response) {
 
     var query = new AV.Query(GoldenWeek);
     query.equalTo('type', type);
+
     
-    query.first({
-        success: function(data) {
-            if(!data) {
-                response.error('查询失败，没有找到匹配的兑换码，请重新输入');
-                return;
-            }
-
-
-            var post = new GoldenWeek();
-            post.set("code", "12345");
-            post.set("type", "222");
-            post.save(null, {
-                success: function() {
-                },
-                error: function(err) {
-                }
-            });
- 
-            
-            data.set('userId',userId);
-            data.set('purchaseId',purchaseId);
-            data.set('type',type);
-            
-            response.success(data);
+    var post = new GoldenWeek();
+    post.set("code", "12345");
+    post.set("type", "222");
+    post.save(null, {
+        success: function() {
+            response.success('okkokokk');
         },
-        error: function(error) {
-            response.error("Error: " + error.code + " " + error.message);
+        error: function(err) {
         }
     });
+    
+//    query.first({
+//        success: function(data) {
+//            if(!data) {
+//                response.error('查询失败，没有找到匹配的兑换码，请重新输入');
+//                return;
+//            }
+//
+//
+//            
+// 
+//            
+//            data.set('userId',userId);
+//            data.set('purchaseId',purchaseId);
+//            data.set('type',type);
+//            
+//            response.success(data);
+//        },
+//        error: function(error) {
+//            response.error("Error: " + error.code + " " + error.message);
+//        }
+//    });
     
 });
 
