f822c39b4cbb13f2213d0a0f21631b2535775fa0
ralf <kingarthas37@hotmail.com>
2015-09-11 16:18:16 +0800

    dev

--- cloud.js
+++ cloud.js
@@ -4,6 +4,8 @@ var AV = require('leanengine');
 
 AV.Cloud.define('hello', function (request, response) {
 
+    var userId = request.params.userId;
+    var purchaseId = request.params.purchaseId;
     var type = request.params.type;
 
     var query = new AV.Query('GoldenWeek');
@@ -11,20 +13,21 @@ AV.Cloud.define('hello', function (request, response) {
     
     query.first({
         success: function(data) {
-            
             if(!data) {
                 response.error('查询失败，没有找到匹配的兑换码，请重新输入');
                 return;
             }
             
-            response.success(data.get('code'));
+            data.set('userId',userId);
+            data.set('purchaseId',purchaseId);
+            data.set('type',type);
             
+            response.success(data);
         },
         error: function(error) {
             response.error("Error: " + error.code + " " + error.message);
         }
     });
-    
     
 });
 
