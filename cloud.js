2b653552e7cca0a548354538c5eff3a123577817
ralf <kingarthas37@hotmail.com>
2015-09-13 21:29:28 +0800

    dev2

--- cloud.js
+++ cloud.js
@@ -1,5 +1,6 @@
 var AV = require('leanengine');
 var GoldenWeek = AV.Object.extend('GoldenWeek');
+var UserWeek = AV.Object.extend('UserWeek');
 
 
 //笨鸟，八达
@@ -9,38 +10,69 @@ AV.Cloud.define('type1', function (request, response) {
     var purchaseId = request.params.purchaseId || '';
     var type = request.params.type || '';
     
-    var query = new AV.Query(GoldenWeek);
-    
-    query.equalTo('type', type);
-    query.equalTo('userId',userId);
-    query.equalTo('purchaseId',purchaseId);
+    var userQuery = new AV.Query(UserWeek);
 
-    query.find({
+    userQuery.equalTo('type', type);
+    userQuery.equalTo('userId',userId);
+    userQuery.equalTo('purchaseId',purchaseId);
+
+
+    userQuery.first({
         success: function(data) {
             
-            if(data[0]) {    
+            if(data[0]) {
                 response.success({
                     success:0,
                     msg:'您已领取优惠码'
                 });
             } else {
+                
+                var codeQuery = new AV.Query(GoldenWeek);
+                codeQuery.equalTo('type',type);
+                codeQuery.first({
+                    success: function(_data) {
 
-                var goldenWeek = new GoldenWeek();
-                goldenWeek.set('type', type);
-                goldenWeek.set('userId', userId);
-                goldenWeek.set('purchaseId', purchaseId);
-                goldenWeek.save(null, {
-                    success: function() {
+                        userQuery.save(null, {
+                            success: function (__data) {
+                                response.success({
+                                    success: 1,
+                                    msg: '领取优惠码成功！',
+                                    data: __data
+                                });
+                            },
+                            error: function (err) {
+                                response.error(err);
+                            }
+                        });
+                        
+                        
                         response.success({
                             success:1,
                             msg:'领取优惠码成功！',
-                            data:data
+                            data:_data
                         });
                     },
                     error: function(err) {
                         response.error(err);
                     }
                 });
+                
+//                var goldenWeek = new GoldenWeek();
+//                goldenWeek.set('type', type);
+//                goldenWeek.set('userId', userId);
+//                goldenWeek.set('purchaseId', purchaseId);
+//                goldenWeek.save(null, {
+//                    success: function() {
+//                        response.success({
+//                            success:1,
+//                            msg:'领取优惠码成功！',
+//                            data:data
+//                        });
+//                    },
+//                    error: function(err) {
+//                        response.error(err);
+//                    }
+//                });
                 
             }
             
