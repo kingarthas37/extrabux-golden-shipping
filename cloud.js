a1be53229a45a13baf724d562976adb6627abb4a
ralf <kingarthas37@hotmail.com>
2015-09-13 15:48:41 +0800

    dev

--- cloud.js
+++ cloud.js
@@ -4,11 +4,14 @@ var GoldenWeek = AV.Object.extend('GoldenWeek');
 
 AV.Cloud.define('bada', function (request, response) {
 
-    var userId = request.body.userId;
-    var purchaseId = request.body.purchaseId;
-    var type = request.body.type;
+    var userId = request.params.userId;
+    var purchaseId = request.params.purchaseId;
+    var type = request.params.type;
 
+    response.success(request);
+    
     var query = new AV.Query(GoldenWeek);
+    
     query.equalTo('type', type);
 
     var post = new GoldenWeek();
