38c488b38cb391137d0d5afc8f51a8b6e0fa1027
ralf <kingarthas37@hotmail.com>
2015-09-14 10:47:56 +0800

    dev3

--- cloud.js
+++ cloud.js
@@ -8,16 +8,8 @@ require('./cloud/type2');
 
 
 AV.Cloud.define('test', function (request, response) {
-
-    var userId = request.query.userId || '';
-    var purchaseId = request.query.purchaseId || '';
-    var type = request.query.type || '';
     
-    response.success({
-        userId:userId,
-        type:type,
-        purchaseId:purchaseId
-    });
+    response.success('test ok');
 
 });
 
