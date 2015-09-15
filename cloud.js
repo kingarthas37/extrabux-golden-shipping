51c9dfc8c6aad06754bbcf2f6d7ac0a717bbf61b
ralf <kingarthas37@hotmail.com>
2015-09-15 11:01:46 +0800

    dev3

--- cloud.js
+++ cloud.js
@@ -1,6 +1,8 @@
 'use strict';
 var AV = require('leanengine');
 
+var crypto = require('crypto');
+var signature = crypto.createHash('sha256').update('kingarthas').digest('hex');
 
 require('./cloud/type1');
 require('./cloud/type2');
@@ -8,6 +10,12 @@ require('./cloud/type3');
 
 AV.Cloud.define('test', function (request, response) {
     response.success('test ok');
+});
+
+AV.Cloud.define('signature',function(request,response) {
+    
+    response.success(signature + ',kingarthas');
+    
 });
 
 module.exports = AV.Cloud;
