a97ef55352ab7b34b44d3089d222da3034fd91db
ralf <kingarthas37@hotmail.com>
2015-09-15 11:38:25 +0800

    dev3

--- cloud.js
+++ cloud.js
@@ -1,6 +1,5 @@
 'use strict';
 var AV = require('leanengine');
- 
 
 require('./cloud/type1');
 require('./cloud/type2');
@@ -9,28 +8,5 @@ require('./cloud/type3');
 AV.Cloud.define('test', function (request, response) {
     response.success('test ok');
 });
-
-
-AV.Cloud.define('signature',function(request,response) {
-
-    var userId = request.params.userId || '';
-    var purchaseId = request.params.purchaseId || '';
-    var signature = request.params.signature || '';
-    
-    if(signature === getSignature(userId,purchaseId)) {
-        response.success(signature + ',' + getSignature(userId,purchaseId) + ',' + 'yes');
-    } else {
-        response.success(signature + ',' + getSignature(userId,purchaseId) + ',' + 'no');
-    }
-    
-    response.success(signature + ',kingarthas');
-    
-});
-
-
-function getSignature(userId,purchaseId) {
-    var str = userId + ' ' + purchaseId + ' ' +  'extrabux2015-luck123';
-    return crypto.createHash('sha256').update(str).digest('hex');
-}
 
 module.exports = AV.Cloud;
