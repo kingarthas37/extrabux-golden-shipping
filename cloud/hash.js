176e47c0bbce65cb469d10ba37d9503e3aff48c9
ralf <kingarthas37@hotmail.com>
2015-09-15 11:42:13 +0800

    dev4

--- cloud/hash.js
+++ cloud/hash.js
@@ -3,5 +3,5 @@ var crypto = require('crypto');
 module.exports = function(signature,userId,purchaseId) {
     var str = userId + ' ' + purchaseId + ' ' +  'extrabux2015-luck123';
     var hash = crypto.createHash('sha256').update(str).digest('hex');
-    return hash === str;
+    return hash === signature;
 };
