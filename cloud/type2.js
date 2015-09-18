aae1fdc383253c474ae85646cc036f51b0f48132
ralf <kingarthas37@hotmail.com>
2015-09-18 10:25:59 +0800

    dev5

--- cloud/type2.js
+++ cloud/type2.js
@@ -6,8 +6,52 @@ var UserWeek = AV.Object.extend('GoldenUser');
 
 var hash = require('./hash');
 
+
 //天际
 AV.Cloud.define('type2', function (request, response) {
+
+    var userId = request.params.userId || '';
+    var purchaseId = request.params.purchaseId || '';
+    var type = request.params.type || '';
+
+    var codeQuery = new AV.Query(GoldenWeek);
+    codeQuery.equalTo('type', type);
+
+    codeQuery.first({
+        success: function (_data) {
+
+            var codeQuerySaveUser = new UserWeek();
+            codeQuerySaveUser.set('code', _data.get('code'));
+            codeQuerySaveUser.set('type', type);
+            codeQuerySaveUser.set('userId', userId);
+            codeQuerySaveUser.set('purchaseId', purchaseId);
+            codeQuerySaveUser.save(null, {
+                success: function () {
+                    _data.destroy().then(function() {
+                        response.success({
+                            success: 1,
+                            msg: '领取优惠码成功！',
+                            code:_data.get('code')
+                        });
+                    });
+                },
+                error: function (err) {
+                    response.error(err);
+                }
+            });
+
+        },
+        error: function (err) {
+            response.error(err);
+        }
+    });
+
+
+});
+
+
+//天际
+AV.Cloud.define('type2-bak', function (request, response) {
 
     var userId = request.params.userId || '';
     var purchaseId = request.params.purchaseId || '';
