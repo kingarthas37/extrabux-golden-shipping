20f8c298b2d2591b12ee9b6870ace48288104520
ralf <kingarthas37@hotmail.com>
2015-10-23 17:02:00 +0800

    dev5

--- cloud.js
+++ cloud.js
@@ -4,6 +4,7 @@ var AV = require('leanengine');
 require('./cloud/type1');
 require('./cloud/type2');
 require('./cloud/type3');
+require('./cloud/type4');
 
 AV.Cloud.define('test', function (request, response) {
     response.success('test ok');
