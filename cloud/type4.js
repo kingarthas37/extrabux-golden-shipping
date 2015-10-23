f7331a4eb5918462ddf4792644a3078f16ccc109
ralf <kingarthas37@hotmail.com>
2015-10-23 16:56:19 +0800

    dev5

--- cloud/type4.js
+++ cloud/type4.js
@@ -9,7 +9,7 @@ var hash = require('./hash');
 
 
 //2015 Black Friday 斑马
-AV.Cloud.define('2015-banma', function (request, response) {
+AV.Cloud.define('2015-BM', function (request, response) {
 
     var userId = request.params.userId || '';
     var type = request.params.type || '';
