e0930ee5112b9d0ebeeddd807c2a88e558d88e2d
ralf <kingarthas37@hotmail.com>
2015-10-23 15:18:49 +0800

    dev5

---
+++ cloud/type4.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var AV = require('leanengine');
+var BlackFriday = AV.Object.extend('BlackFriday');
+var BlackFridayUser = AV.Object.extend('BlackFridayUser');
+
+var hash = require('./hash'); 
+
+
+
+//2015 Black Friday 斑马
+AV.Cloud.define('2015-banma', function (request, response) {
+
+    var userId = request.params.userId || '';
+    var type = request.params.type || '';
+
+    var codeQuery = new AV.Query(BlackFriday);
+    codeQuery.equalTo('type', type);
+
+    codeQuery.first({
+        success: function (_data) {
+
+            var codeQuerySaveUser = new BlackFridayUser();
+            codeQuerySaveUser.set('code', _data.get('code'));
+            codeQuerySaveUser.set('type', type);
+            codeQuerySaveUser.set('userId', userId);
+            codeQuerySaveUser.save(null, {
+                success: function () {
+                    response.success({
+                        success: 1,
+                        msg: '领取优惠码成功！',
+                        code:_data.get('code')
+                    });
+                },
+                error: function (err) {
+                    response.error(err);
+                }
+            });
+
+        },
+        error: function (err) {
+            response.error(err);
+        }
+    });
+    
+
+
+});
