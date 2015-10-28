7fb6e87e00dc92ade3c4709ca08bdd3145fcb624
ralf <kingarthas37@hotmail.com>
2015-10-28 16:47:37 +0800

    dev8

--- cloud.js
+++ cloud.js
@@ -1,7 +1,12 @@
 'use strict';
 var AV = require('leanengine');
 
+require('./cloud/type1');
+require('./cloud/type2');
+require('./cloud/type3');
 
+require('./cloud/promo-2015-blackfriday-banma');
+ 
 
 AV.Cloud.define('test', function (request, response) {
     response.success('test ok');
